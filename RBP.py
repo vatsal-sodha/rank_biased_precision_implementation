98397418855c419ff7575046b7e1c61180016c19
vatsal-sodha <vatsalsodha150297@gmail.com>
2017-07-27 10:49:15 +1000

    added Average in RBP.py

--- RBP.py
+++ RBP.py
@@ -22,6 +22,8 @@ def RBP_eval(p,qrel,trecFile,gmax=1,save=None):
 		print("QId"+" "+"RBP Score")
 	previousQid=""
 	total=0.0
+	grandTotal=0.0
+	noOfQuery=0
 	index=0
 	for i,row in enumerate(trecFileList):
 		row=row.split(" ")
@@ -34,17 +36,24 @@ def RBP_eval(p,qrel,trecFile,gmax=1,save=None):
 		else:
 			# print("index is %d" %index)
 			total=(1-float(p))*total
+			grandTotal+=total
 			if save==None:
 				print(previousQid+" "+"{0:.4f}".format(total))
 			else:
 				saveFileObject.write(previousQid+" "+"{0:.4f}".format(total)+"\n")
 			index=0
 			total=0.0
+			noOfQuery+=1
 		previousQid=qrelRow[0]
 	total=(1-float(p))*total
+	grandTotal+=total
+	noOfQuery+=1
 	if save== None:
 		print(row[0]+" "+"{0:.4f}".format(total))
+		print("Average is {0:.4f}".format(grandTotal/noOfQuery))
 	else:
 		saveFileObject.write(previousQid+" "+"{0:.4f}".format(total)+"\n")
+		saveFileObject.write("Average is {0:.4f}".format(grandTotal/noOfQuery))
+
 		saveFileObject.close()
 baker.run()
